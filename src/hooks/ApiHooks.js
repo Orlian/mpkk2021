d5255119aa38825a92774fc10e4dc0ad6114431e
Joonas <asd@asd.fi>
2021-03-23 13:28:19 +0200

    hooksit done

---
+++ src/hooks/ApiHooks.js
@@ -0,0 +1,23 @@
+import {baseUrl} from '../utils/variables';
+import {useState, useEffect} from 'react';
+
+const useAllMedia = () => {
+  const [picArray, setPicArray] = useState([]);
+  useEffect(() => {
+    const loadMedia = async () => {
+      const response = await fetch(baseUrl + 'media');
+      const files = await response.json();
+      console.log(files);
+      const media = await Promise.all(files.map(async (item) => {
+        const resp = await fetch(baseUrl + 'media/' + item.file_id);
+        return resp.json();
+      }));
+
+      setPicArray(media);
+    };
+    loadMedia();
+  }, []);
+  return picArray;
+};
+
+export {useAllMedia};
